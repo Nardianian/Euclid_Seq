910583369b87e3d096b4bea540366c845019ebcd
Nardianian <145938851+Nardianian@users.noreply.github.com>
2025-12-20 23:57:03 +0100

    Refactor Euclidean class into EuclideanPattern

--- Source/utils/euclidean.h
+++ Source/utils/euclidean.h
@@ -4,73 +4,61 @@
     euclidean.h
     Created: 9 Oct 2022 9:05:42pm
     Author:  Andreas Sandersen
-
+    Modified: 13 Dec 2025 0.10:00 am by Nard.
   ==============================================================================
 */
-
 #pragma once
-#include <JuceHeader.h>
+#include <vector>
+#include <cmath>
 
-class euclidean
+class EuclideanPattern
 {
-    
 public:
-    euclidean();
-    ~euclidean() override;
-    
-    //Euclidean algorithm
-    std::string euclidean(int pulses, int steps)
+    EuclideanPattern() = default;
+
+    void setPattern(int steps, int pulses)
     {
-        //We can only have as many beats as we have steps (0 <= beats <= steps)
-        if (pulses > steps)
-            pulses = steps;
-
-        //Each iteration is a process of pairing strings X and Y and the remainder from the pairings
-        //X will hold the "dominant" pair (the pair that there are more of)
-        std::string x = "1";
-        int x_amount = pulses;
-
-        std::string y = "0";
-        int y_amount = steps - pulses;
-
-        do
-        {
-            //Placeholder variables
-            int x_temp = x_amount;
-            int y_temp = y_amount;
-            std::string y_copy = y;
-
-            //Check which is the dominant pair
-            if (x_temp >= y_temp)
-            {
-                //Set the new number of pairs for X and Y
-                x_amount = y_temp;
-                y_amount = x_temp - y_temp;
-
-                //The previous dominant pair becomes the new non dominant pair
-                y = x;
-            }
-            else
-            {
-                x_amount = x_temp;
-                y_amount = y_temp - x_temp;
-            }
-
-            //Create the new dominant pair by combining the previous pairs
-            x = x + y_copy;
-        } while (x_amount > 1 && y_amount > 1);//iterate as long as the non dominant pair can be paired (if there is 1 Y left, all we can do is pair it with however many Xs are left, so we're done)
-
-                                               //By this point, we have strings X and Y formed through a series of pairings of the initial strings "1" and "0"
-                                               //X is the final dominant pair and Y is the second to last dominant pair
-        std::string rhythm;
-        for (int i = 1; i <= x_amount; i++)
-            rhythm += x;
-        for (int i = 1; i <= y_amount; i++)
-            rhythm += y;
-        return rhythm;
+        numSteps = steps;
+        numPulses = pulses;
+        generate();
+        currentStep = 0;
     }
-    
+
+    bool nextStep()
+    {
+        if (pattern.empty()) return false;
+        bool result = pattern[currentStep];
+        currentStep = (currentStep + 1) % numSteps;
+        return result;
+    }
+
+    void reset() { currentStep = 0; }
+
+    const std::vector<bool>& getPattern() const { return pattern; }
+
 private:
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Arp)
+    int numSteps = 16;
+    int numPulses = 4;
+    int currentStep = 0;
+    std::vector<bool> pattern;
+
+    void generate()
+    {
+        pattern.clear();
+        pattern.resize(numSteps, false);
+        if (numPulses <= 0) return;
+
+        int bucket = 0;
+        for (int i = 0; i < numSteps; ++i)
+        {
+            bucket += numPulses;
+            if (bucket >= numSteps)
+            {
+                bucket -= numSteps;
+                pattern[i] = true;
+            }
+        }
+    }
 };
+#pragma once
+
