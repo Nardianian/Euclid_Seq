483bb660cbf958759edc5640b34912a693bc6d3d
Nardianian <145938851+Nardianian@users.noreply.github.com>
2025-12-21 13:24:25 +0100

    Remove header comments and include directive

--- Source/MIDI/MidiGenerator.cpp
+++ Source/MIDI/MidiGenerator.cpp
@@ -1,11 +1,3 @@
 /*
-  ==============================================================================
-    MidiGenerator.cpp
-    Created: 9 Oct 2022 9:09:12pm
-    Author:  Andreas Sandersen
-    Modified: 13 Dec. 2025 00.25:00 am by Nard.
-  ==============================================================================
-*/
-
 #include "MidiGenerator.h"
-// Al momento tutto inline, non servono implementazioni aggiuntive
+// At the moment everything is inline, no additional implementations are needed
